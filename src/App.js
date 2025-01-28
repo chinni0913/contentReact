c038ccfd314531be3e0a214eb3173300a1401774
unknown <shreya>
2025-01-28 18:58:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cards from './Components/Cards';
+
+function App() {
+  return (
+    <div className="App">
+       <Cards />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Cards.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './Cards.css';
+
+const Cards = () => {
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://www.reddit.com/r/reactjs.json')
+      .then(response => response.json())
+      .then(data => {
+        setPosts(data.data.children.map(child => child.data));
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Posts</h1>
+      <div className="cards">
+        {posts.map((post, index) => (
+          <div className="card" key={index}>
+            <h2 style={{backgroundColor:'teal', color:'#fff', padding:"5px", borderRadius:'5px'}}>{post.title}</h2>
+            <div className="score">Score: {post.score}</div>
+            {post.selftext_html && <div className="text" dangerouslySetInnerHTML={{ __html: post.selftext_html }} />}
+            <a href={post.url} target="_blank" rel="noopener noreferrer" className="url">
+              <button className='btn btn-primary btn-sm'>Read More</button>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
